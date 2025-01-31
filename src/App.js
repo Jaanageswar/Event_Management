e145ef924a98e4eb668634a58aa9fbafd06c08cf
Jaanageswar <jaanageswar2002@gmail.com>
2025-01-31 09:45:21 +0530

    Changed Browserrouter to hashrouter

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Navbar from './Components/Navbar/Navbar';
 import Home from './Components/Home/Home';
 import Booking from './Components/Booking/Booking';
 import Footer from './Components/Footer/Footer';
-import { BrowserRouter as Router, Route,Routes} from 'react-router-dom';
+import { HashRouter as Router, Route,Routes} from 'react-router-dom';
 
 
 function App() {
